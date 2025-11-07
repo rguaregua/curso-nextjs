a43f36bd45db7ca20fa166bfc5d5c4ece7bf3aee
guaregua00 <romel174gl@gmail.com>
2025-11-07 11:46:26 -0400

    Final curso se agrego el active link en navbar

--- clase-01/introduccion/components/navbar/Navbar.tsx
+++ clase-01/introduccion/components/navbar/Navbar.tsx
@@ -1,10 +1,29 @@
+"use client";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 export const Navbar = () => {
+    const pathname = usePathname();
     return (
         <nav className="flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded">
             <span className="mr-2">App</span>
-            <a className="mr-2" href="/about">About</a>
-            <a className="mr-2" href="/pricing">Pricing</a>
-            <a className="mr-2" href="/contact">Contact</a>
+                <ul style={{ display: "flex", gap: "1rem", listStyle: "none", padding: 0 }}>
+                    <li>
+                        <Link href="/about" className={pathname === "/about" ? "active" : ""}>
+                            About
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/pricing" className={pathname === "/pricing" ? "active" : ""}>
+                            Pricing
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/contact" className={pathname === "/contact" ? "active" : ""}>
+                            Contact
+                        </Link>
+                    </li>
+                </ul>
         </nav>
     )
 }
